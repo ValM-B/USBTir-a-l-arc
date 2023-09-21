12ef832620d590d3e3e1c31fc1bb797017111e13
ValM-B <valerie.maurice@oclock.school>
2023-09-21 15:56:11 +0200

    add all tarifs and course types in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,44 @@
 
 namespace App\DataFixtures;
 
+use App\DataFixtures\Provider\OtirProvider;
+use App\Entity\CourseType;
+use App\Entity\Tarif;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $provider = new OtirProvider();
+
+        //Tarifs
+        foreach ($provider->prices() as $price) {
+            $newPrice = new Tarif;
+            $newPrice->setName($price["name"]);
+            $newPrice->setAgeMin($price["ageMin"]);
+            $newPrice->setAgeMax($price["ageMax"]);
+            $newPrice->setAmount($faker->numberBetween(80,200));
+            $newPrice->setGearAmount(30);
+            $newPrice->setCreatedAt(new DateTimeImmutable());
+            $manager->persist($newPrice);
+        }
+
+        //Course Types
+        $courseTypes = [];
+        foreach ($provider->courseTypes() as $type) {
+           $newType = new CourseType;
+           $newType->setName($type);
+           $newType->setCreatedAt(new DateTimeImmutable());
+           $courseTypes[] = $newType;
+           $manager->persist($newType);
+        }
+
+
 
         $manager->flush();
     }
--- src/DataFixtures/Povider/OtirProvider.php
+++ src/DataFixtures/Povider/OtirProvider.php
@@ -3,5 +3,65 @@ namespace App\DataFixtures\Provider;
 
 class OtirProvider
 {
+	private $prices = [
+		[ 
+			"name" => "Baby",
+			"ageMin" => 6,
+			"ageMax" => 8,
+		],
+		[ 
+			"name" => "Poussin",
+			"ageMin" => 9,
+			"ageMax" => 11,
+		],
+		[ 
+			"name" => "Jeune Débutant",
+			"ageMin" => 12,
+			"ageMax" => 17,
+		],
+		[ 
+			"name" => "Jeune Confirmé",
+			"ageMin" => 12,
+			"ageMax" => 17,
+		],
+		[ 
+			"name" => "Adulte Débutant",
+			"ageMin" => 18,
+			"ageMax" =>  null,
+		],
+		[ 
+			"name" => "Adulte Confirmé",
+			"ageMin" => 18,
+			"ageMax" =>  null,
+		]
+	];
 
+	private $courseTypes = [
+		"Baby",
+		"Jeunes débutants",
+		"Jeunes perfectionnement",
+		"Jeunes compétiteurs",
+		"Adultes débutants",
+		"Adultes perfectionnement",
+		"Adultes compétiteurs",
+		"Handisport"
+	];
+
+	/**
+     * Return array of prices
+	 * @return array
+     */
+    public function prices()
+    {
+        return $this->prices;
+    }
+
+	/**
+     * Return array of course types
+	 * @return array
+     */
+    public function courseTypes()
+    {
+        return $this->courseTypes();
+    }
 }
