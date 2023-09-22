41a30e0276409ac4a2ae6a754a721f4bc0417f0f
JBMenesguen <jean-baptiste.menesguen@oclock.school>
2023-09-22 16:24:01 +0200

    modifications des tarifs

---
+++ src/Controller/Front/AccessController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccessController extends AbstractController
+{
+    /**
+     * @Route("/access", name="app_access")
+     */
+    public function index(): Response
+    {
+        return $this->render('access/index.html.twig', [
+            'controller_name' => 'AccessController',
+        ]);
+    }
+}
---
+++ src/Controller/Front/TarifController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TarifRepository;
+
+class TarifController extends AbstractController
+{
+    /**
+     * @Route("/tarif", name="app_tarif")
+     */
+    public function index(TarifRepository $tarifRepository): Response
+    {
+        $tarifs = $tarifRepository->findAll();
+
+        return $this->render('tarif/index.html.twig', [
+            'controller_name' => 'TarifController',
+        ]);
+    }
+}
