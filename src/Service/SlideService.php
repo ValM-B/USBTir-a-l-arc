c1144efbca64148627bd057b2ec00b9e34a91363
DylanBru <dylan.bru@gmail.com>
2023-09-25 15:30:33 +0200

    slide bootstrap

--- src/Service/SlideService.php
+++ src/Service/SlideService.php
@@ -4,15 +4,15 @@ namespace App\Service;
 class SlideService
 {
 	private $picturesUrl = [
-		"public/images/competition.jpg",
-		"public/images/coupe-florence-villars.jpg",
-		"public/images/coupe-florence-villars-2.jpg",
-		"public/images/coupe-florence-villars-3.jpg",
-		"public/images/espace-florence-villars.jpg",
-		"public/images/cours-baby.jpg",
-		"public/images/fleches-en-cible.jpg",
-		"public/images/handisport.jpg",
-		"public/images/salle.jpg",
+		"competition.jpg",
+		"coupe-florence-villars.jpg",
+		"coupe-florence-villars-2.jpg",
+		"coupe-florence-villars-3.jpg",
+		"espace-florence-villars.jpg",
+		"cours-baby.jpg",
+		"fleches-en-cible.jpg",
+		"handisport.jpg",
+		"salle.jpg",
 	];
 
 	/**
