e01c776cbcf69640921372da6057724944d2b7a3
JBMenesguen <jean-baptiste.menesguen@oclock.school>
2023-09-25 16:17:09 +0200

    ajout de connaître les infos de contacts dans le MainController

--- src/Controller/Front/TarifController.php
+++ src/Controller/Front/TarifController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TarifRepository;
-use App\Repository\HoraireRepository;
+
 
 class TarifController extends AbstractController
 {
