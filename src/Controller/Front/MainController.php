7b8100e9a3a04b6c785927624fb07deca4cc59b0
JBMenesguen <jean-baptiste.menesguen@oclock.school>
2023-09-26 09:43:17 +0200

    modification MainController

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\UserRepository;
 use App\Service\SlideService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class MainController extends AbstractController
 {
@@ -20,23 +22,20 @@ class MainController extends AbstractController
             "pictures" => $pictures
         ]);
     }
-
-    /**
-     * @Route("/nous-contacter", name="app_access")
-     */
-    public function access(): Response
-    {
-        return $this->render('front/access.html.twig');         
-    }
+    
     /**
      * @Route("/nous-contacter", name="app_contact")
      */
-    public function contact(): Response
+    public function contact(UserRepository $repository): Response
     {
+        $president = $repository->findPresidentPosition();
+        
     return $this->render('front/contact/contact.html.twig', [
         'controller_name' => 'MainController',
+        'president' => $president   
     ]);
-}
+    }
+
     /**
      * @Route("/mentions-legales", name="app_legal_notice")
      */
