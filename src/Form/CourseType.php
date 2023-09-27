0d7fad29cdb96cc49928b70433c2c0e69fc7156a
DylanBru <dylan.bru@gmail.com>
2023-09-27 13:21:39 +0200

    crud for backoffice

---
+++ src/Form/CourseType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Course;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('day')
+            ->add('hour')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('courseType')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Course::class,
+        ]);
+    }
+}
