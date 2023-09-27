9aa5225ab759cf61eb55793a07663dd48be43b87
ValM-B <valerie.maurice@oclock.school>
2023-09-27 17:13:05 +0200

    creation of UserController in Api

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/v1")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/users", name="api_v1_users")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+}
