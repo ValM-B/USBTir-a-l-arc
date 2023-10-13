42a2e32ac692c628ca9c1a9a119b02bbe6cbb11f
DylanBru <dylan.bru@gmail.com>
2023-10-13 13:19:08 +0200

    smoke test for public pages

---
+++ tests/SmokeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SmokeTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+        yield ['/horaires-et-tarifs'];
+        yield ['/nous-contacter'];
+        yield ['/mentions-legales'];
+        yield ['/connexion'];
+    }
+}
