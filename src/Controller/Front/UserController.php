a77236cfebaa03c2c01abed735b2ed0ac8cc9281
ValM-B <valerie.maurice@oclock.school>
2023-11-13 15:59:05 +0100

    add subscriber to check email in user form email

--- src/Controller/Front/UserController.php
+++ src/Controller/Front/UserController.php
@@ -6,10 +6,12 @@ namespace App\Controller\Front;
 use App\Form\UserEmailType;
 use App\Form\UserPasswordType;
 use App\Repository\UserRepository;
+use App\Service\FormVerificatorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DomCrawler\Form;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -18,34 +20,40 @@ class UserController extends AbstractController
     /**
      * @Route("/profil", name="app_user", methods={"GET", "POST"})
      */
-    public function show(HttpFoundationRequest $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
+    public function show(HttpFoundationRequest $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, FormVerificatorService $formVerificator): Response
     {
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
         
-        // Create and handle the email update form
+        // Create the email update form
         $formEmail = $this->createForm(UserEmailType::class, $user);
         $formEmail->handleRequest($request);
+        // Create the password update form
+        $formPassword = $this->createForm(UserPasswordType::class, $user);
+        $formPassword->handleRequest($request);
+
+        // Handle the email update form
         if ($formEmail->isSubmitted()) {
-            if($formEmail->isValid()){
-                     $userRepository->add($user, true);
+            if($formEmail->isValid()) {
 
-            $this->addFlash(
-                'success',
-                "La mise à jour a bien été effectuée."
+                $userRepository->add($user, true);
+                $this->addFlash(
+                    'success',
+                    "La mise à jour a bien été effectuée."
                 );
+                return $this->redirectToRoute('app_user', [], Response::HTTP_SEE_OTHER);
 
-            return $this->redirectToRoute('app_user', [], Response::HTTP_SEE_OTHER);
             } else {
                 $this->addFlash(
                     'danger',
                     "La mise à jour n'a pas pu être effectuée."
                 );
             }
-       
         }
+       
+        
 
-        // Create and handle the password update form
+        // Handle the password update form
         $formPassword = $this->createForm(UserPasswordType::class, $user);
         $formPassword->handleRequest($request);
         if ($formPassword->isSubmitted()) {
